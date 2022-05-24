c3a604459efa14e14f850f05ef053d16f57fe838
Jacob Lee <JLpython@outlook.com>
2022-05-24 08:18:54 -0700

    feat: create module for web scraping utilities

---
+++ sabrmetrics/sites/_scrape.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+
+import typing
+
+import bs4
+import requests
+
+
+def get_soup(url: str) -> typing.Optional[bs4.BeautifulSoup]:
+    """
+
+    """
+    res = requests.get(url)
+    if res.status_code != 200:
+        return
+    soup = bs4.BeautifulSoup(res.text, features="lxml")
+    return soup

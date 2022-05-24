f6da5b9198f12f59f8b483aff489450a35283eab
Jacob Lee <JLpython@outlook.com>
2022-05-24 08:06:52 -0700

    feat: URLs for Baseball Reference players pages

---
+++ sabrmetrics/sites/baseball_reference/_urls.py
@@ -0,0 +1,27 @@
+"""
+
+"""
+
+
+class URLs:
+    """
+
+    """
+    def __init__(self):
+        self._base_url = "https://www.baseball-reference.com/"
+
+        # players
+        self._player_index = "players/{letter}"
+        self._player = "players/{letter}/{id}.shtml"
+
+    @property
+    def base_url(self) -> str:
+        return self._base_url
+
+    @property
+    def player_index(self) -> str:
+        return self._base_url + self._player_index
+
+    @property
+    def player(self) -> str:
+        return self._base_url + self._player

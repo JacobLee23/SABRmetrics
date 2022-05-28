47185eaba9f31612546ae3d81d5743300865f0ed
Jacob Lee <JLpython@outlook.com>
2022-05-28 14:08:10 -0700

    test: split test players into batters and pitchers

--- sabrmetrics/tests/test_sites/__init__.py
+++ sabrmetrics/tests/test_sites/__init__.py
@@ -4,9 +4,14 @@
 
 import os
 
-
-PLAYERS_PATH = os.path.join(
-    "sabrmetrics", "tests", "test_sites", "players.txt"
+_DIRECTORY = os.path.join(
+    "sabrmetrics", "tests", "test_sites", "test_baseball_reference"
 )
-with open(PLAYERS_PATH, "r", encoding="utf-8") as file:
-    PLAYERS = file.read().split("\n")
+BATTERS_PATH = os.path.join(_DIRECTORY, "test_batters.txt")
+PITCHERS_PATH = os.path.join(_DIRECTORY, "test_pitchers.txt")
+
+with open(BATTERS_PATH, "r", encoding="utf-8") as file:
+    BATTERS = file.read().split("\n")
+with open(PITCHERS_PATH, "r", encoding="utf-8") as file:
+    PITCHERS = file.read().split("\n")
+PLAYERS = BATTERS + PITCHERS

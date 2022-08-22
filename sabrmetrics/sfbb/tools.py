ebd90c0507cd7827e0afa84b7319e7dff7af9cc3
Jacob Lee <JLpython@outlook.com>
2022-08-22 11:08:22 -0700

    feat: create sabrmetrics.sfbb.tools.PlayerIDMap class to scrape the 'Player ID Map' section of the 'Tools' webpage

--- sabrmetrics/sfbb/tools.py
+++ sabrmetrics/sfbb/tools.py
@@ -2,7 +2,31 @@
 
 """
 
+import requests
+
 from sabrmetrics.sfbb._headers import HEADERS
 
 
 URL = "https://www.smartfantasybaseball.com/tools/"
+
+
+class PlayerIDMap:
+    """
+
+    """
+    @property
+    def headers(self) -> dict[str, str]:
+        """
+
+        :return:
+        """
+        return HEADERS
+
+    @property
+    def response(self) -> requests.Response:
+        """
+
+        :return:
+        """
+        return requests.get(URL, headers=self.headers)
+
--- sabrmetrics/tests/test_sfbb/test_tools.py
+++ sabrmetrics/tests/test_sfbb/test_tools.py
@@ -2,6 +2,7 @@
 Tests for :py:mod:`sabrmetrics.sfbb.tools`.
 """
 
+import pytest
 import requests
 
 from sabrmetrics.sfbb import tools
@@ -13,3 +14,24 @@ def test_url():
     """
     with requests.get(tools.URL, headers=tools.HEADERS) as response:
         assert response.status_code == 200
+
+
+class TestPlayerIDMap:
+    """
+    Tests for :py:class:`sabrmetrics.sfbb.tools.PlayerIDMap`.
+    """
+    x = tools.PlayerIDMap()
+
+    def test_headers(self):
+        """
+        Unit test for :py:attr:`sabrmetrics.sfbb.tools.PlayerIDMap.headers`.
+
+        Tested in :py:func:`sabrmetrics.tests.test_sfbb.test_headers.test_headers`.
+        """
+        pytest.skip()
+
+    def test_response(self):
+        """
+        Unit test for :py:attr:`sabrmetrics.sfbb.tools.PlayerIDMap.response`.
+        """
+        assert self.x.response.status_code == 200

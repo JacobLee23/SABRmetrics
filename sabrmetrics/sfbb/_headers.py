eb3164e139024a2a55a60f5c3900015a63642fcb
Jacob Lee <JLpython@outlook.com>
2022-08-22 10:39:12 -0700

    feat: read HTTP request headers from JSON file

--- sabrmetrics/sfbb/_headers.py
+++ sabrmetrics/sfbb/_headers.py
@@ -2,7 +2,27 @@
 
 """
 
+import json
 import pathlib
 
 
-PATH = pathlib.Path("sabrmetrics", "sfbb", "data", "headers.json")
+class Headers:
+    """
+
+    """
+    @property
+    def path(self) -> pathlib.Path:
+        """
+
+        :return:
+        """
+        return pathlib.Path("sabrmetrics", "sfbb", "data", "headers.json")
+
+    @property
+    def content(self) -> dict:
+        """
+
+        :return:
+        """
+        with open(self.path, "r", encoding="utf-8") as file:
+            return json.load(file)
--- sabrmetrics/tests/test_sfbb/test_headers.py
+++ sabrmetrics/tests/test_sfbb/test_headers.py
@@ -2,12 +2,34 @@
 Tests for :py:mod:`sabrmetrics.sfbb._headers`.
 """
 
+import pathlib
+
 from sabrmetrics.sfbb import _headers
 
 
-def test_path():
+class TestHeaders:
     """
+    Tests for :py:class:`sabrmetrics.sfbb._headers.Headers`.
+    """
+    x = _headers.Headers()
 
-    """
-    assert _headers.PATH.exists()
-    assert _headers.PATH.suffix == ".json"
+    _headers_list_path = pathlib.Path("sabrmetrics", "tests", "test_sfbb", "headers_list.txt")
+    with open(_headers_list_path, "r", encoding="utf-8") as file:
+        headers_list = file.read().split("\n")
+
+    def test_path(self):
+        """
+        Test for :py:attr:`sabrmetrics.sfbb._headers.Headers.path`.
+        """
+        assert self.x.path.exists()
+        assert self.x.path.suffix == ".json"
+
+    def test_content(self):
+        """
+        Test for :py:attr:`sabrmetrics.sfbb._headers.Headers.content`.
+        """
+        assert isinstance(self.x.content, dict)
+        assert all(isinstance(k, str) for k in self.x.content.keys())
+        assert all(isinstance(v, str) for v in self.x.content.values())
+
+        assert all(k in self.headers_list for k in self.x.content.keys())

166cbbde76e6a12ff87269c0f595ff1c73eff4ec
Jacob Lee <JLpython@outlook.com>
2022-08-23 14:55:11 -0700

    test: update tests for sabrmetrics.sfbb._headers

--- sabrmetrics/tests/test_sfbb/test_headers.py
+++ sabrmetrics/tests/test_sfbb/test_headers.py
@@ -10,34 +10,6 @@ import requests
 from sabrmetrics.sfbb import _headers
 
 
-class TestHeaders:
-    """
-    Tests for :py:class:`sabrmetrics.sfbb._headers.Headers`.
-    """
-    x = _headers.Headers()
-
-    _headers_list_path = pathlib.Path("sabrmetrics", "tests", "test_sfbb", "headers_list.txt")
-    with open(_headers_list_path, "r", encoding="utf-8") as file:
-        headers_list = file.read().split("\n")
-
-    def test_path(self):
-        """
-        Unit test for :py:attr:`sabrmetrics.sfbb._headers.Headers.path`.
-        """
-        assert self.x.path.exists()
-        assert self.x.path.suffix == ".json"
-
-    def test_content(self):
-        """
-        Unit test for :py:attr:`sabrmetrics.sfbb._headers.Headers.content`.
-        """
-        assert isinstance(self.x.content, dict)
-        assert all(isinstance(k, str) for k in self.x.content.keys())
-        assert all(isinstance(v, str) for v in self.x.content.values())
-
-        assert all(k in self.headers_list for k in self.x.content.keys())
-
-
 @pytest.mark.parametrize(
     "url", [
         "https://baseball-reference.com",

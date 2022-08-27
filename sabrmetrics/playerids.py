f466cb1ab9c824b6279c9122dc412c7297c01de0
Jacob Lee <JLpython@outlook.com>
2022-08-27 15:53:22 -0700

    feat: initialize sabrmetrics.playerids module

---
+++ sabrmetrics/playerids.py
@@ -0,0 +1,7 @@
+"""
+Wrapper for the various Player ID databases in the codebase.
+
+- `Smart Fantasy Baseball`_: :py:class:`sabrmetrics.sfbb.tools.PlayerIDMap`
+
+.. _Smart Fantasy Baseball: https://smartfantasybaseball.com/
+"""

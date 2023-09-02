1188af85b73484e4bc560318088465d07360b22f
Jacob Lee <JLpython@outlook.com>
2023-09-02 12:28:52 -0700

    refactor: sabrmetrics.mlb.league

--- sabrmetrics/mlb/league.py
+++ sabrmetrics/mlb/league.py
@@ -6,32 +6,22 @@ import typing
 
 import requests
 
-from .address import Address_
+from .address import APIAddress
 from .scraper import APIScraper
 from sabrmetrics import TODAY
 
 
-class Address(Address_):
+class Address(APIAddress):
     """
     """
-    base = "https://statsapi.mlb.com/api/v1/league"
+    url = "https://statsapi.mlb.com/api/v1/league/{league_id}"
+    field_defaults = {"season": TODAY.year}
 
-    class _FieldDefaults(typing.NamedTuple):
+    @property
+    def season(self) -> str:
         """
         """
-        league_id: int = None
-        season: int = TODAY.year
-
-    def concatenate(self) -> str:
-        fpath = []
-        if self["league_id"] is not None:
-            fpath.append(str(self["league_id"]))
-
-        queries = {}
-        if self["season"] is not None:
-            queries.setdefault("season", str(self["season"]))
-
-        return f"{self.base}/{'/'.join(fpath)}?{'&'.join(f'{k}={v}' for k, v in queries.items())}"
+        return str(self.fields["season"])
 
 
 class League(APIScraper):
@@ -62,47 +52,61 @@ class AmericanLeague(League):
     """
     :param season:
     """
+    url = Address.url.format(league_id=103)
+
     def __init__(self, season: int = TODAY.year):
-        super().__init__(103, season)
+        super().__init__(season)
 
 
 class NationalLeague(League):
     """
     :param season:
     """
+    url = Address.url.format(league_id=104)
+
     def __init__(self, season: int = TODAY.year):
-        super().__init__(104, season)
+        super().__init__(season)
 
 
 class CactusLeague(League):
     """
     :param season:
     """
+    url = Address.url.format(league_id=114)
+
     def __init__(self, season: int = TODAY.year):
-        super().__init__(114, season)
+        super().__init__(season)
 
 
 class GrapefruitLeague(League):
     """
     :param season:
     """
+    url = Address.url.format(league_id=115)
+
     def __init__(self, season: int = TODAY.year):
-        super().__init__(115, season)
+        super().__init__(season)
 
 
 class Season:
     """
+    :param year:
+    :param league:
     """
-    def __init__(self, year: int = TODAY.year):
+    date_spans = {
+        "preseaon": ("preSeasonStartDate", "preSeasonEndDate"),
+        "season": ("seasonStartDate", "seasonEndDate"),
+        "spring": ("springStartDate", "springEndDate"),
+        "regular-season": ("regularSeasonStartDate", "regularSeasonEndDate"),
+        "first-half": ("regularSeasonStartDate", "lastDate1stHalf"),
+        "second-half": ("firstDate2ndHalf", "regularSeasonEndDate"),
+        "postseason": ("postSeasonStartDate", "postSeasonEndDate"),
+        "offseason": ("offSeasonStartDate", "offSeasonEndDate")
+    }
+
+    def __init__(self, year: int = TODAY.year, *, league: League = AmericanLeague):
         self._year = year
-
-        league_data = (
-            AmericanLeague(year).data["leagues"][0],
-            NationalLeague(year).data["leagues"][0]
-        )
-        assert league_data[0]["seasonDateInfo"] == league_data[1]["seasonDateInfo"]
-
-        self._data = league_data[0]["seasonDateInfo"]
+        self._data = league(year)["leagues"][0]["seasonDateInfo"]
 
     def __getitem__(self, key: str) -> typing.Union[int, float, str, datetime.datetime]:
         value = self.data[key]
@@ -110,19 +114,58 @@ class Season:
         try:
             return int(value)
         except ValueError:
-            pass
+            try:
+                return float(value)
+            except ValueError:
+                try:
+                    return datetime.datetime.strptime(value, "%Y-%m-%d")
+                except ValueError:
+                    return str(value)
 
-        try:
-            return float(value)
-        except ValueError:
-            pass
+    @classmethod
+    def latest_season(
+        cls, date: datetime.datetime = TODAY, span: str = "regular-season"
+    ) -> "Season":
+        """
+        :param date:
+        :param span:
+        :return:
+        :raise ValueError:
+        """
+        season = cls(date.year)
+        keys = cls.date_spans[span]
+        start = season[keys[0]]
 
-        try:
-            return datetime.datetime.strptime(value, "%Y-%m-%d")
-        except ValueError:
-            pass
+        if datetime.datetime(date.year, 1, 1) <= date < start:
+            return cls(date.year - 1)
+        elif start <= date <= datetime.datetime(date.year, 12, 31):
+            return season
+        else:
+            raise ValueError(date)
 
-        return value
+    @classmethod
+    def latest_year(cls, date: datetime.datetime = TODAY, span: str = "regular-season") -> int:
+        """
+        :param date:
+        :param span:
+        :return:
+        """
+        return cls.latest_season(date, span).year
+
+    @classmethod
+    def latest_date(cls, date: datetime.datetime = TODAY, span: str = "regular-season") -> int:
+        """
+        :param date:
+        :return:
+        :raise ValueError:
+        """
+        season = cls(date.year)
+        keys = cls.date_spans[span]
+        start, end = season[keys[0]], season[keys[1]]
+
+        if start <= date <= end:
+            return date
+        return cls.latest_season(date, span)[keys[1]]
 
     @property
     def data(self) -> dict:
@@ -138,11 +181,23 @@ class Season:
         """
         return self._year
 
-    @classmethod
-    def latest(cls, date: datetime.datetime = TODAY) -> "Season":
+    def date_range(
+        self, span: str = "regular-season"
+    ) -> typing.Tuple[datetime.datetime, datetime.datetime]:
         """
-        :param date:
+        :param span:
         :return:
         """
-        season = cls(TODAY.year)
-        return season if date >= season["seasonStartDate"] else cls(date.year - 1)
+        keys = self.date_spans[span]
+        return self[keys[0]], self[keys[1]]
+
+    def date_in_span(
+        self, date: datetime.datetime = TODAY, span: str = "regular-season"
+    ) -> bool:
+        """
+        :param date:
+        :param span:
+        :return:
+        """
+        start, end = self.date_range(span)
+        return start <= date <= end

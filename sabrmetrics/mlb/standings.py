5829efc6aacd38022d6c2655b5587be27c699661
Jacob Lee <JLpython@outlook.com>
2023-09-05 13:17:18 -0700

    feat: update sabrmetrics.mlb.Standings to scraper MLB advanced standings data

--- sabrmetrics/mlb/standings.py
+++ sabrmetrics/mlb/standings.py
@@ -5,6 +5,7 @@
 import datetime
 import typing
 
+import numpy as np
 import pandas as pd
 
 from . import divisions
@@ -116,6 +117,7 @@ class Standings(APIScraper):
 
         self._dataframe = pd.concat(pd.DataFrame(x["teamRecords"]) for x in self._records)
         self._dataframe.reset_index(drop=True, inplace=True)
+        self._dataframe.replace("-", np.nan, inplace=True)
 
     @property
     def team(self) -> pd.DataFrame:
@@ -135,14 +137,125 @@ class Standings(APIScraper):
         """
         return pd.DataFrame(list(self._dataframe.loc[:, "leagueRecord"]))
 
-    def standings(self) -> pd.DataFrame:  
+    @property
+    def split_records(self) -> pd.DataFrame:
         """
+        """
+        return self._flat_record(
+            list(self._dataframe.loc[:, "records"]), "splitRecords"
+        )
+
+    @property
+    def division_records(self) -> pd.DataFrame:
+        """
+        """
+        return self._nested_record(
+            list(self._dataframe.loc[:, "records"]), "divisionRecords", "division"
+        )
+    
+    @property
+    def overall_records(self) -> pd.DataFrame:
+        """
+        """
+        return self._flat_record(
+            list(self._dataframe.loc[:, "records"]), "overallRecords"
+        )
+    
+    @property
+    def league_records(self) -> pd.DataFrame:
+        """
+        """
+        return self._nested_record(
+            list(self._dataframe.loc[:, "records"]), "leagueRecords", "league"
+        )
+
+    @property
+    def expected_records(self) -> pd.DataFrame:
+        """
+        """
+        return self._flat_record(
+            list(self._dataframe.loc[:, "records"]), "expectedRecords"
+        )
+
+    def standings(
+        self, *,
+        advanced: typing.Literal["split", "division", "overall", "league", "expected"] = None,
+        streak: bool = True,
+        league_record: bool = True
+    ) -> pd.DataFrame:  
+        """
+        :param advanced:
+        :param streak:
+        :param league_record:
         :return:
         """
-        return pd.concat(
-            [
-                self.team, self._dataframe.loc[:, "season"], self.streak,
-                self._dataframe.loc[:, "divisionRank":"leagueRecord"], self.league_record,
-                self._dataframe.loc[:, "runsAllowed":]
-            ], axis=1
+        df_standard = pd.concat(
+            [self._dataframe.drop(columns=["team", "streak", "leagueRecord", "records"])],
+            keys=["standard"], axis=1
         )
+        df_team = pd.concat([self.team], keys=["team"], axis=1)
+
+        dataframe = pd.concat([df_team, df_standard], axis=1)
+            
+        if advanced == "split":
+            dataframe = dataframe.join(self.split_records)
+        elif advanced == "division":
+            dataframe = dataframe.join(self.division_records)
+        elif advanced == "overall":
+            dataframe = dataframe.join(self.overall_records)
+        elif advanced == "league":
+            dataframe = dataframe.join(self.league_records)
+        elif advanced == "expected":
+            dataframe = dataframe.join(self.expected_records)
+        
+        if streak:
+            dataframe = dataframe.join(
+                pd.concat([self.streak], keys=["streak"], axis=1)
+            )
+        if league_record:
+            dataframe = dataframe.join(
+                pd.concat([self.league_record], keys=["leagueRecord"], axis=1)
+            )
+
+        return dataframe
+
+    def _flat_record(
+        self, records: typing.List[typing.Dict], key: str
+    ) -> pd.DataFrame:
+        """
+        :param records:
+        :param key:
+        :return:
+        """
+        series = []
+
+        for record in records:
+            dataframe = pd.DataFrame(record[key])
+
+            dataframe.rename(index=dataframe.loc[:, "type"], inplace=True)
+            dataframe.drop(columns=["type"], inplace=True)
+
+            series.append(dataframe.stack())
+
+        return pd.DataFrame(series)
+    
+    def _nested_record(
+        self, records: typing.List[typing.Dict], key: str, inner_key: str
+    ) -> pd.DataFrame:
+        """
+        :param records:
+        :param key:
+        :param inner_key:
+        :return:
+        """
+        series = []
+
+        for record in records:
+            dataframe = pd.DataFrame(record[key])
+
+            nested_df = pd.DataFrame(list(dataframe.loc[:, inner_key]))
+            nested_df.columns = pd.MultiIndex.from_product([[inner_key], nested_df.columns])
+
+            series.append(pd.concat([dataframe.iloc[:, :-1], nested_df], axis=1).stack())
+
+        return pd.DataFrame(series)
